016a16612d91fb5072c15060a6455bf1d9e227d4
Josh K <keisler@live.com>
2025-03-29 14:50:36 +0000

    Add ssl to db connection

--- app/settings.py
+++ app/settings.py
@@ -102,8 +102,7 @@ DATABASES = {
     #     "PORT": os.getenv("DB_PORT"),
     # },
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
+        default=os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True
     )
 }
 
